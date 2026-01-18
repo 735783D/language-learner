20daf4ce8d4adc114fb77a2d830ddf4b85277c1e
735783D <88239246+735783D@users.noreply.github.com>
2026-01-18 02:08:27 -0600

    Got the reptiles/amphibians and aquatic animals done for sound. Will work on a different set next time since the remainders are quite large. Next up, record more animal sounds.

--- src/data/creek/vocabulary/animals/aquatic.js
+++ src/data/creek/vocabulary/animals/aquatic.js
@@ -1,10 +1,10 @@
 export const aquatic = [
-  { mvskoke: "Est-akwvnayv", english: "Octopus", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Kvlfo", english: "Catfish", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Osanv", english: "Otter", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Eccaswv", english: "Beaver", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Svntalakwv", english: "Perch", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Calo", english: "Bass/trout", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Sakco", english: "Crawdad", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Rvro", english: "Fish", image: "", image_alt: "", sound_file: "" }
+  { mvskoke: "Est-akwvnayv", english: "Octopus", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/EST-AKWVNAYV(Octopus).mp3" },
+  { mvskoke: "Kvlfo", english: "Catfish", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/KVLFO(Catfish).mp3" },
+  { mvskoke: "Osanv", english: "Otter", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/OSANV(Otter).mp3" },
+  { mvskoke: "Eccaswv", english: "Beaver", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/ECCASWV(Beaver).mp3" },
+  { mvskoke: "Svntalakwv", english: "Perch", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/SVNTALAKWV(Perch).mp3" },
+  { mvskoke: "Calo", english: "Bass/trout", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/CALO(Bass_Trout).mp3" },
+  { mvskoke: "Sakco", english: "Crawdad", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/SAKCO(Crawdad).mp3" },
+  { mvskoke: "Rvro", english: "Fish", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/aquatic/RVRO(Fish).mp3" }
 ];
--- src/data/creek/vocabulary/animals/reptiles.js
+++ src/data/creek/vocabulary/animals/reptiles.js
@@ -1,12 +1,12 @@
 // REPTILES & AMPHIBIANS (10 reptiles)
 export const reptiles = [
-  { mvskoke: "Hvyakpo cetto", english: "King snake", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Sokhv Hatkv", english: "Opossum", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Taksv Pulkv", english: "Lizard", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Cetto", english: "Snake", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Hvlpvtv", english: "Alligator", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Locv", english: "Turtle", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Kute", english: "Frog", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Hvrpe-tvkvcwe", english: "Armadillo", image: "", image_alt: "", sound_file: "" },
-  { mvskoke: "Hvlpvtv-Yopo-Fvske", english: "Crocodile", image: "", image_alt: "", sound_file: "" }
+  { mvskoke: "Hvyakpo cetto", english: "King snake", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/HVYAKPO_CETTO(King_Snake).mp3" },
+  { mvskoke: "Sokhv Hatkv", english: "Opossum", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/SOKHV_HATKV(Opossum).mp3" },
+  { mvskoke: "Taksv Pulkv", english: "Lizard", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/TAKSV_PULKV(Lizard).mp3" },
+  { mvskoke: "Cetto", english: "Snake", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/CETTO(Snake).mp3" },
+  { mvskoke: "Hvlpvtv", english: "Alligator", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/HVLPVTV(Alligator).mp3" },
+  { mvskoke: "Locv", english: "Turtle", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/LOCV(Turtle).mp3" },
+  { mvskoke: "Kute", english: "Frog", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/KUTE(Frog).mp3" },
+  { mvskoke: "Hvrpe-tvkvcwe", english: "Armadillo", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/HVRPE-TVKVCWE(Armadillo).mp3" },
+  { mvskoke: "Hvlpvtv-Yopo-Fvske", english: "Crocodile", image: "", image_alt: "", sound_file: "/language-learner/sounds/creek/nouns/animals/reptiles_amphibians/HVLPVTV-YOPO-FVSKE(Crocodile).mp3" }
 ];
