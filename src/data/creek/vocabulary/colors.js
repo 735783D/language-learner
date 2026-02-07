a1cb8ffdbe35bf0bf090e12a71c8d8bbf0b965dc
735783D <88239246+735783D@users.noreply.github.com>
2026-02-06 23:53:26 -0600

    Did the audio for family and colors and deployed to prod. Next up, do audio for food and then deploy again.

--- src/data/creek/vocabulary/colors.js
+++ src/data/creek/vocabulary/colors.js
@@ -5,77 +5,77 @@ export const colors = [
     english: "Black",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/LVSTE(Black).mp3"
   },
   { 
     mvskoke: "Hvtke", 
     english: "White",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/HVTKE(White).mp3"
   },
   { 
     mvskoke: "Holatte", 
     english: "Blue",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/HOLATTE(Blue).mp3"
   },
   { 
     mvskoke: "Cate", 
     english: "Red",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/CATE(Red).mp3"
   },
   { 
     mvskoke: "Lane", 
     english: "Yellow",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/LANE(Yellow).mp3"
   },
   { 
     mvskoke: "Pvhe-lane", 
     english: "Green",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/PVHE-LANE(Green).mp3"
   },
   { 
     mvskoke: "Pvrko-ome", 
     english: "Purple",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/PVRKO-OME(Purple).mp3"
   },
   { 
     mvskoke: "Yvlahv-ome", 
     english: "Orange",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/YVLAHV-OME(Orange).mp3"
   },
   { 
     mvskoke: "Cate-ome", 
     english: "Pink",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/CATE-OME(Pink).mp3"
   },
   { 
     mvskoke: "Sopakhvtke", 
     english: "Grey",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/SOPAKHVTKE(Grey).mp3"
   },
   { 
     mvskoke: "Oklane", 
     english: "Brown",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/colors/OKLANE(Brown).mp3"
   }
 ];
 
--- src/data/creek/vocabulary/family.js
+++ src/data/creek/vocabulary/family.js
@@ -5,161 +5,161 @@ export const family = [
     english: "Mother",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECKE(Mother).mp3"
   },
   { 
     mvskoke: "Erke", 
     english: "Father",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ERKE(Father).mp3"
   },
   { 
     mvskoke: "Eppuce", 
     english: "a man's son",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/EPPUCE(SonOfAMan).mp3"
   },
   { 
     mvskoke: "Eccuste", 
     english: "a man's daughter",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECCUSTE(DaughterOfAMan).mp3"
   },
   { 
     mvskoke: "Eccuswv", 
     english: "a woman's son or daughter",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECCUSWV(SonOrDaughterOfAWoman).mp3"
   },
   { 
     mvskoke: "Emososwv", 
     english: "grandchild",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/EMOSOSWV(Grandchild).mp3"
   },
   { 
     mvskoke: "Ervhv", 
     english: "Older Sibling (same gender as you)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ERVHV(OlderSiblingSameGender).mp3"
   },
   { 
     mvskoke: "Vne", 
     english: "Younger Sibling (same gender as you)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/VNE(YoungerSiblingSameGender).mp3"
   },
   { 
     mvskoke: "Ecuse", 
     english: "A man's sister",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECUSE(MansSister).mp3"
   },
   { 
     mvskoke: "Ecerwv", 
     english: "A woman's brother",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECERWV(MansBrother).mp3"
   },
   { 
     mvskoke: "Ewvnwv", 
     english: "Sibling (gender neutral)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/EWVNWV(SiblingGenderNeutral).mp3"
   },
   { 
-    mvskoke: "etecakkat", 
+    mvskoke: "Etecakkat", 
     english: "Sibling (gender neutral)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ETECACKKAT(SiblingGenderNeutral).mp3"
   },
   { 
     mvskoke: "Hvkpvte", 
     english: "Niece",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/HVKPVTE(Niece).mp3"
   },
   { 
     mvskoke: "Hopuewv", 
     english: "Nephew",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/HOPUEWV(Nephew).mp3"
   },
   { 
     mvskoke: "Eckuce", 
     english: "Aunt (maternal)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECKUCE(MaternalAndPaternalAunt).mp3"
   },
   { 
     mvskoke: "Pvwv", 
     english: "Uncle (maternal)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/PVWV(MaternalUncle).mp3"
   },
   { 
     mvskoke: "Eckuce", 
     english: "Aunt (paternal)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ECKUCE(MaternalAndPaternalAunt).mp3"
   },
   { 
     mvskoke: "Erkuce", 
     english: "Uncle (paternal)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ERKUCE(PaternalUncle).mp3"
   },
   { 
     mvskoke: "Enahvmke", 
     english: "Cousin",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ENAHVMKE(Cousin).mp3"
   },
   { 
     mvskoke: "Puca", 
     english: "Grandpa",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/PUCA(Grandpa).mp3"
   },
   { 
     mvskoke: "Puse", 
     english: "Grandma",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/PUSE(Grandma).mp3"
   },
   { 
     mvskoke: "Encukotekv", 
     english: "family/household",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/ENCUKOTEKV(Family_HouseHold).mp3"
   },
   { 
     mvskoke: "Cuko-hvmecvlke", 
     english: "family/household",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/family/CUKO-HVMECVLKE(Family_HouseHold).mp3"
   }
 ];
 
