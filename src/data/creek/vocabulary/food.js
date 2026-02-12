f7b053ee9cb7920adeb221fc0adb3ed95c1f6ade
735783D <88239246+735783D@users.noreply.github.com>
2026-02-12 02:01:03 -0600

    Did the audio for food while I was hungry. Not a good idea. Deployed to prod. Next up, body parts audio.

--- src/data/creek/vocabulary/food.js
+++ src/data/creek/vocabulary/food.js
@@ -5,182 +5,182 @@ export const food = [
     english: "Corn",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/VCE(Corn).mp3"
   },
   { 
     mvskoke: "Vpvske", 
     english: "Parched corn drink",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/VPVSKE(Parched-corn-drink).mp3"
   },
   { 
     mvskoke: "(o)safke", 
     english: "Corn drink",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/SAFKE(Corn-drink).mp3"
   },
   { 
     mvskoke: "Taklik kvmokse", 
     english: "Sour cornbread",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/TAKLIK-KVMOKSE(Sour-cornbread).mp3"
   },
   { 
     mvskoke: "Cvtv hakv", 
     english: "Blue bread",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/CVTV-HAKV(Blue-bread).mp3"
   },
   { 
     mvskoke: "Tafvmpuce", 
     english: "Wild onions",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/TAFVMPUCE(Wild-onions).mp3"
   },
   { 
     mvskoke: "Cvse", 
     english: "Pumpkin",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/CVSE(Pumpkin).mp3"
   },
   { 
     mvskoke: "Sokhv vpeswv sakmorke", 
     english: "Fried pork",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/SOKHV-VPESWV-SAKMORKE(Fried-pork).mp3"
   },
   { 
     mvskoke: "Tvlako", 
     english: "Beans",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/TVLAKO(Beans).mp3"
   },
   { 
     mvskoke: "Pvrko sakpoloke", 
     english: "Grape dumplings (V1)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/PVRKO-SAKPOLOKE(Grape-dumplings).mp3"
   },
   { 
     mvskoke: "Pvrko afke", 
     english: "Grape dumplings (v2)",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/PVRKO-AFKE(Grape-dumplings).mp3"
   },
   { 
     mvskoke: "Tosenv", 
     english: "Salt meat",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/TOSENv(Salt-meat).mp3"
   },
   { 
     mvskoke: "Taklik sakmorke", 
     english: "Fry bread",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/TAKLIK-SAKMORKE(Fry-bread).mp3"
   },
   { 
     mvskoke: "Sokhv vpeswv sokv", 
     english: "Pork and hominy",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/SOKHV-VPESWV-SOKV(Pork-and-hominy).mp3"
   },
   { 
     mvskoke: "Sakkonepke", 
     english: "Hominy and meat",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/SAKKONEPKE(Hominy-and-meat).mp3"
   },
   { 
     mvskoke: "Weso", 
     english: "Sassafras",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/WESO(Sassafras).mp3"
   },
   { 
     mvskoke: "Ecko", 
     english: "Dried corn",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/ECKO(Dried-corn).mp3"
   },
   { 
     mvskoke: "Vpvtvkv", 
     english: "Stove-top pancake",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/VPVTVKV(Stove-top-pancake).mp3"
   },
   { 
     mvskoke: "Svtv Rakko", 
     english: "apple",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/SVTV-RAKKO(Apple).mp3"
   },
   { 
     mvskoke: "Pvkanv", 
     english: "Peach",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/PVKANV(Peach).mp3"
   },
   { 
     mvskoke: "Pvrko", 
     english: "Grapes",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/PVRKO(Grapes).mp3"
   },
   { 
     mvskoke: "Svtarkomv", 
     english: "Pear",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/SVTARKOMV(Pear).mp3"
   },
   { 
     mvskoke: "Yvlahv", 
     english: "Orange",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/YVLAHV(Orange).mp3"
   },
   { 
     mvskoke: "Kepalv", 
     english: "Strawberry",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/KEPALV(Strawberry).mp3"
   },
   { 
     mvskoke: "Cvstvle", 
     english: "Watermelon",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/CVSTVLE(Watermelon).mp3"
   },
   { 
     mvskoke: "Fvmecv", 
     english: "Cantaloupe",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/food/FVMECV(Cantaloupe).mp3"
   }
 ];
 
