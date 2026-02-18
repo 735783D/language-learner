9eb61b6a0ca351b2b355293db180b87c13b954b9
735783D <88239246+735783D@users.noreply.github.com>
2026-02-18 04:07:37 -0600

    Did the audio for the body parts. Deployed to prod. Next up, audio for nature. Then I think it is back to crafting this app into something actually with some code!

--- src/data/creek/vocabulary/body.js
+++ src/data/creek/vocabulary/body.js
@@ -4,140 +4,140 @@ export const body = [
     english: "Body",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ENA(Body).mp3"
   },
   { 
     mvskoke: "Ekv", 
     english: "Head",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/EKV(Head).mp3"
   },
   { 
     mvskoke: "Ekv-esse", 
     english: "Hair",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/EKV-ESSE(Hair).mp3"
   },
   { 
     mvskoke: "Turwv", 
     english: "Eyes",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/TURWV(Eyes).mp3"
   },
   { 
     mvskoke: "Hvcko", 
     english: "Ears",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/HVCKO(Ears).mp3"
   },
   { 
     mvskoke: "Yupo", 
     english: "Nose",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/YUPO(Nose).mp3"
   },
   { 
     mvskoke: "Cukwv", 
     english: "Mouth",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/CUKWV(Mouth).mp3"
   },
   { 
     mvskoke: "Nute", 
     english: "Teeth",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/NUTE(Teeth).mp3"
   },
   { 
     mvskoke: "Tulaswv", 
     english: "Tongue",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/TULASWV(Tongue).mp3"
   },
   { 
     mvskoke: "Enke", 
     english: "Hands",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ENKE(Hands).mp3"
   },
   { 
     mvskoke: "Esakpv", 
     english: "Arms",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ESAKPV(Arms).mp3"
   },
   { 
     mvskoke: "Enke-Wesakv", 
     english: "Fingers",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ENKE-WESAKV(Fingers).mp3"
   },
   { 
     mvskoke: "Elle", 
     english: "Legs",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ELLE(Legs).mp3"
   },
   { 
     mvskoke: "Elle-Wesakv", 
     english: "Toes",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ELLE-WESAKV(Toes).mp3"
   },
   { 
     mvskoke: "Elle-Tvpekse", 
     english: "Feet",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ELLE-TVPEKSE(Feet).mp3"
   },
   { 
     mvskoke: "Etorkowv", 
     english: "Knee",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ETORKOWV(Knee).mp3"
   },
   { 
     mvskoke: "Envrke", 
     english: "Stomach",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ENVRKE(Stomach).mp3"
   },
   { 
     mvskoke: "Enukwv", 
     english: "Neck",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ENUKWV(Neck).mp3"
   },
   { 
     mvskoke: "Era", 
     english: "Back",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ERA(Back).mp3"
   },
   { 
     mvskoke: "Enokfepkv", 
     english: "Throat",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/body-parts/ENOKFEPKV(Throat).mp3"
   },
 ];
 
