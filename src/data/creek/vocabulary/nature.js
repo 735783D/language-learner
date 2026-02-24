51dfdc9046f6b8cd4a4fd1990f21d768df38da7f
735783D <88239246+735783D@users.noreply.github.com>
2026-02-24 01:31:08 -0600

    Did the audio for nature and deployed to prod. I think that's all the audio I want to do for now. Next up, let's build out some more shit to make this thing something people can actually learn from while enjoying it.

--- src/data/creek/vocabulary/nature.js
+++ src/data/creek/vocabulary/nature.js
@@ -5,98 +5,98 @@ export const nature = [
     english: "Rain",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/OSKE(Rain).mp3"
   },
   { 
     mvskoke: "Hetute", 
     english: "Snow",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HETUTE(Snow).mp3"
   },
   { 
     mvskoke: "Vholoce", 
     english: "Cloudy",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/VHOLOCHE(Cloudy).mp3"
   },
   { 
     mvskoke: "Tenetke", 
     english: "Thunder",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/TENETKE(Thunder).mp3"
   },
   { 
     mvskoke: "Vtokyehatte", 
     english: "Lightning",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/VTOKYEHATTE(Lightning).mp3"
   },
   { 
     mvskoke: "Hotvle rakko", 
     english: "Tornado",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HOTVLE-RAKKO(Tornado).mp3"
   },
   { 
     mvskoke: "Hotvle", 
     english: "Wind / Windy",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HOTVLE(Windy).mp3"
   },
   { 
     mvskoke: "Hetute nerkuce", 
     english: "Sleet",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HETUTE-NERKUCE(Sleet).mp3"
   },
   { 
     mvskoke: "Kvsvppe", 
     english: "Cold",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/KVSVPPE(Cold).mp3"
   },
   { 
     mvskoke: "Hiye", 
     english: "Hot",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HIYE(Hot).mp3"
   },
   { 
     mvskoke: "Hvsotte", 
     english: "Sunshine / Sunny",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HVSOTTE(Sunshine-Sunny).mp3"
   },
   { 
     mvskoke: "Here", 
     english: "Good",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/HERE(Good).mp3"
   },
   { 
     mvskoke: "Fettv stonko?", 
     english: "How is the weather?",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/FETTV-STONKO(How-is-the-weather).mp3"
   },
   { 
     mvskoke: "Lekothe", 
     english: "Warm",
     image: "",
     image_alt: "",
-    sound_file: ""
+    sound_file: "/language-learner/sounds/creek/nouns/nature/LEKOTHE(Warm).mp3"
   }
 ];
 
